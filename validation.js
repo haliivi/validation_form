3d63334bf4320534b4684685ed05f4554eda7a64
AlDS90 <lkjs0JH)0fw3vcSFDSF9>
2021-12-09 13:27:44 +0700

    Finish.

--- validation.js
+++ validation.js
@@ -1,1 +1,29 @@
-console.log('Hello')
+// var reg = /[a-z]/gi;
+
+// var reg_2 = new RegExp(/[a-z]/, 'g')
+
+'use strict';
+
+const inputs = document.querySelectorAll('input')
+
+const patterns = {
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+).([a-z]{2,8})(\.[a-z]{2,8})?$/
+}
+
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid'
+    } else {
+        field.className = 'invalid'
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
